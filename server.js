614242441db59c7c8d3b6b87e6b167768ac65be2
Sheriffuddeen09 <odukoyasheriff@gmail.com>
2025-05-29 20:52:59 -0700

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
-const router = jsonServer.router("db.json")
+const router = jsonServer.router("data.json")
 
 const PORT = process.env.PORT || 8080;
 
